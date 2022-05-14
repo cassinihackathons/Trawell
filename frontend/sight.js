cb50248def15acc514e7f9d1f4f528e222609152
Adrian Widlak <adrian.widlak@codecool.com>
2022-05-14 02:39:33 +0200

    Create card, and dice rollout

---
+++ frontend/sight.js
@@ -0,0 +1,40 @@
+const dice = document.querySelector('.dice');
+
+dice.addEventListener('click', () => {
+    getData();
+});
+
+function getData() {
+    const url = `http://192.168.0.206:8080/location/attraction?long=50.061886117001634&lat=19.93860295517751`;
+    fetch(url, {
+            mode: 'cors',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            createCard(
+                data.attraction.image,
+                data.attraction.name,
+                data.attraction.score,
+            );
+        });
+}
+
+function createCard(src, cardTitle, score) {
+    const card = document.querySelector(".card");
+    const template = document.querySelector('#subcard');
+    const clone = template.content.cloneNode(true);
+    const img = clone.querySelector("img");
+    const title = clone.querySelector("#title");
+    const distance = clone.querySelector("#distance");
+    const points = clone.querySelector("#points");
+
+    img.src = src;
+    title.innerText = cardTitle;
+    points.innerText = score;
+
+    card.innerHTML = '';
+    card.appendChild(clone);
+}
--- readme.md
+++ readme.md
@@ -1,6 +1,18 @@
 ## PROJECT
 ![alt-text-1](mockups/dashboard.png "dashboard") ![alt-text-2](mockups/login.png "login") ![alt-text-3](mockups/travell_finished.png "travell_finished")
 
+## INSTALLATION
+
+BACKEND:
+
+`python -m venv .venv`
+`source .venv/bin/activate`
+`pip3 install -r requirements.txt`
+`python app.py`
+
+FRONTEND:
+
+E.g. you can run frontend/index.html with LiveServer plugin.
 
 ## LICENSE
 
